9e0ccc502d560dce241ac6e5e4b180ba47e3230c
Laksh-Chawla <laksh2997@gmail.com>
2025-10-21 17:02:22 +0530

    Add Day 17: Populate question bank with data from question_data

--- 100 Days Bootcamp/Day 17/main.py
+++ 100 Days Bootcamp/Day 17/main.py
@@ -1,2 +1,7 @@
 from question_model import Question
 from data import question_data
+
+question_bank = []
+for item in question_data:
+    question_bank.append(Question(item["text"], item["answer"]))
+print(question_bank)
